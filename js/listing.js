be3879b36f246a59ab8195f9aaa8c149fb926b18
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2014-07-06 11:39:34 +0100

    Added key creation

--- js/listing.js
+++ js/listing.js
@@ -26,11 +26,42 @@ $( document ).ready(function(){
 
   $('[name="list-keys"]').on('ajax:success', function(ajax,response,status){
     $('#central-col').empty();
-    $.do.common.loadColumn('keys', response.ssh_keys, 'central');
+    $('#central-col').append($('<p id="central-new-key-col"></p>'));
+    $.do.common.loadColumn('key', response.ssh_keys, 'central');
+  });
+
+  $( 'body' ).on("do:key:column:loaded", function() {
+    var createKeyButton = $('<p id="createKey"><button type="button" class="btn btn-info">Create Key</button></p>');
+    createKeyButton.on('click', function() {
+      $('#createKey').remove();
+      $.do.common.loadColumn('newkey', {}, 'central-new-key');
+      $( 'body' ).on("do:newkey:column:loaded", function() {
+        $('#new-key-form').on('ajax:success', function(ajax, response, status) {
+          $('#central-col').mustache("keytemplate", [response.ssh_key], { method: 'prepend' });
+
+          $.do.common.messageDisplay('success', 'Key created!');
+
+          $('.dlt-key').on('ajax:success', function() {
+            $.do.common.messageDisplay('success', 'Key deleted!');
+            $(this).closest('.well').remove();
+          });
+
+          $(this).parent().remove();
+        });
+      });
+    });
+
+    $('#central-col').prepend(createKeyButton);
+
+    $('.dlt-key').on('ajax:success', function() {
+      $.do.common.messageDisplay('success', 'Key deleted!');
+      $(this).closest('.well').remove();
+    });
   });
 
   $('.dlt-droplet').on('ajax:success', function() {
     $.do.common.messageDisplay('success', 'Droplet deleted!');
     $(this).closest('.well').remove();
   });
+
 });
