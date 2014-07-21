7ad3b05286a0a4e0ea213e3c095dee77ab7b16c0
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2014-07-21 09:34:48 +0100

    Domains get records from subqueries

--- js/domains.js
+++ js/domains.js
@@ -1,8 +1,13 @@
 $( document ).ready(function(){
   $('[name="list-domains"]').on('ajax:success', function(ajax,response,status){
     $('#central-col').empty();
+    $.each(response.domains, function(index, domain){
+      $.do.common.simpleGET('https://api.digitalocean.com/v2/domains/'+domain.name+'/records', {}, function(data, status, xhr){
+        domain.dns = data.domain_records;
+        $.do.common.loadColumn('domain', domain, 'central');
+      });
+    });
     // $('#central-col').append($('<p id="central-new-droplet-col"></p>'));
-    $.do.common.loadColumn('domain', response.domains, 'central');
   });
 
   $( 'body' ).on("do:domain:column:loaded", function() {
