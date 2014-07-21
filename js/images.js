d3e69f6fb06a2d9f0404225b5bdd593b9ecaa276
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2014-07-21 09:34:22 +0100

    Can transfer images between regions

--- js/images.js
+++ js/images.js
@@ -1,16 +1,29 @@
 $( document ).ready(function(){
-  $('[name="list-images"]').on('ajax:success', function(ajax,response,status){
-    var privimages = [];
-    var pubimages  = [];
-    $.each(response.images, function(index, image){
-      if (!image.public){
-        privimages.push(image);
-      } else {
-        pubimages.push(image);
-      }
+  $('[name="list-images"]').on('ajax:success', function(ajax, response, status){
+
+
+    $.do.common.simpleGET('https://api.digitalocean.com/v2/regions', {}, function(data, status, xhr){
+      var privimages = [];
+      var pubimages  = [];
+      $.each(response.images, function(index, image){
+        if (!image.public){
+          var regions = $.extend(true, [], data.regions);
+          $.each(regions, function(index,region){
+            if ($.inArray(region.slug, image.regions) >= 0) {
+              region.disabled = true;
+            } else {
+              region.disabled = false;
+            }
+          });
+          image.globalregions = regions;
+          privimages.push(image);
+        } else {
+          pubimages.push(image);
+        }
+      });
+      $('#central-col').empty();
+      $.do.common.loadColumn('imagesgroups', {privimages: privimages, pubimages: pubimages}, 'central');
     });
-    $('#central-col').empty();
-    $.do.common.loadColumn('imagesgroups', {privimages: privimages, pubimages: pubimages}, 'central');
   });
 
   $( 'body' ).on("do:imagesgroups:column:loaded", function() {
