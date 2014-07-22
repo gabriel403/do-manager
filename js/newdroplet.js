109a6754c85d92a5475ece6c11a69a1dbe905d8b
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2014-07-22 17:09:56 +0100

    Add ssh_keys for droplet creation

--- js/newdroplet.js
+++ js/newdroplet.js
@@ -25,44 +25,48 @@ $.do.newDroplet = function(){
       $.do.common.simpleGET('https://api.digitalocean.com/v2/sizes',   {}, function(data, status, xhr){
         datas.sizes = data.sizes;
 
-        $.do.common.regionsSizes = datas;
-        $.do.common.loadColumn('newdroplet', datas, 'central-new-droplet');
-        $('#central-new-droplet-col').on('do:newdroplet:column:loaded', function(){
-          $('#createDroplet').remove();
-          $('.popover-trigger').popover({});
+        $.do.common.simpleGET('https://api.digitalocean.com/v2/account/keys', {}, function(data, status, xhr){
+          $.do.common.regionsSizes = datas;
+          datas.keys = data.ssh_keys;
 
-          $('#new-droplet-form').on('ajax:success', function(ajax, response, status) {
-            $('#central-col').mustache("droplettemplate", [response.droplet], { method: 'prepend' });
+          $.do.common.loadColumn('newdroplet', datas, 'central-new-droplet');
+          $('#central-new-droplet-col').on('do:newdroplet:column:loaded', function(){
+            $('#createDroplet').remove();
+            $('.popover-trigger').popover({});
 
-            $.do.common.messageDisplay('success', 'Droplet created!');
+            $('#new-droplet-form').on('ajax:success', function(ajax, response, status) {
+              $('#central-col').mustache("droplettemplate", [response.droplet], { method: 'prepend' });
 
-            $('.dlt-droplet').on('ajax:success', function() {
-              $.do.common.messageDisplay('success', 'Droplet deleted!');
-              $(this).closest('.well').remove();
+              $.do.common.messageDisplay('success', 'Droplet created!');
+
+              $('.dlt-droplet').on('ajax:success', function() {
+                $.do.common.messageDisplay('success', 'Droplet deleted!');
+                $(this).closest('.well').remove();
+              });
+
+              $(this).parent().remove();
             });
 
-            $(this).parent().remove();
-          });
+            $('input[name=region]').on('change', function(e){
+              $('input[name=size]').attr('disabled', false);
 
-          $('input[name=region]').on('change', function(e){
-            $('input[name=size]').attr('disabled', false);
-
-            // selecting a region will invalidate some sizes
-            $.each($.do.common.regionsSizes.regions, function(index, region){
-              if (region.slug == $('input[name=region]:checked').val()) {
-                $('input[name=size]').not('input[name=size][value=' + region.sizes.join('],[value=') + ']').attr('disabled', true);
-              }
+              // selecting a region will invalidate some sizes
+              $.each($.do.common.regionsSizes.regions, function(index, region){
+                if (region.slug == $('input[name=region]:checked').val()) {
+                  $('input[name=size]').not('input[name=size][value=' + region.sizes.join('],[value=') + ']').attr('disabled', true);
+                }
+              });
             });
-          });
 
-          $('input[name=size]').on('change', function(e){
-            $('input[name=region]').attr('disabled', false);
+            $('input[name=size]').on('change', function(e){
+              $('input[name=region]').attr('disabled', false);
 
-            // selecting a size will invalidate some regions
-            $.each($.do.common.regionsSizes.sizes, function(index, size){
-              if (size.slug == $('input[name=size]:checked').val()) {
-                $('input[name=region]').not('input[name=region][value=' + size.regions.join('],[value=') + ']').attr('disabled', true);
-              }
+              // selecting a size will invalidate some regions
+              $.each($.do.common.regionsSizes.sizes, function(index, size){
+                if (size.slug == $('input[name=size]:checked').val()) {
+                  $('input[name=region]').not('input[name=region][value=' + size.regions.join('],[value=') + ']').attr('disabled', true);
+                }
+              });
             });
           });
         });
